f8107d1875b9d490b96aaab6185e7b2ce380f583
Anika Tan <anika.tanyy@gmail.com>
2024-10-12 01:02:59 +0800

    challenge 3??

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def main():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/login", methods=["GET", "POST"])
+def logic():
+    return render_template("superLogin.html")
+
+@app.route("/superlogin", methods=["GET", "POST"])
+def super_login():
     # SQL Injection
-    username = request.form['username']
-    password = request.form['pwd']
-    conn = sqlite3.connect("database.db")
-    c = conn.cursor()
-    sql_statement = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
-    results = c.execute(sql_statement).fetchall()
-    return results
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['pwd']
+        conn = sqlite3.connect("database.db")
+        c = conn.cursor()
+        sql_statement = f"SELECT * FROM superusers WHERE username = '{username}' AND password = '{password}'"
+        results = c.execute(sql_statement).fetchall()
+        # return results
+        return render_template("superLogin.html", results=results)
+    else:
+        return render_template("superLogin.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
