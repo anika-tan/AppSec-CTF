15e64ece73e6254331085716f672119d36d28e09
Bryan <bryanlu.my@gmail.com>
2024-10-11 12:44:51 +0800

    renamed stuff and fix bugs

--- ctf_dashboard_backend/app.py
+++ ctf_dashboard_backend/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, send_from_directory
 import os
-import dotenv
+from dotenv import dotenv_values
 
-dotenv.load_dotenv()
+config = dotenv_values(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '.env'))
 
 # Serve from react build folder
-build_path = os.getenv('BRIEFING_BUILD_PATH', os.path.join(
-    'challenge_briefings_and_hints', 'dist'))
+build_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), config.get(
+    'REACT_BUILD_PATH', '../ctf_dashboard_frontend/dist')))
+
+print("Build path: ", build_path)
+
 app = Flask(__name__, static_folder=build_path, static_url_path='')
 
 
@@ -25,4 +29,4 @@ def serve_static_files(path):
 
 if __name__ == "__main__":
     # Default port is 3000
-    app.run(debug=True, port=os.getenv('BRIEFING_PORT', 3000))
+    app.run(debug=True, port=config.get('PORT', 3000))
