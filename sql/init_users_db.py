235170f434549bd0b0fdf1bf9c87d46b19413041
Anika Tan <anika.tanyy@gmail.com>
2024-10-11 20:45:58 +0800

    new branch

--- sql/init_users_db.py
+++ sql/init_users_db.py
@@ -15,7 +15,7 @@ cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
 
 # User information for admin user
 # password is secret. hash taken is MD5. https://md5decrypt.net/en/ 
-cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
+cur.execute("INSERT INTO superusers (username, password) VALUES (?, ?)",
     ('superadmin', '5ebe2294ecd0e0f08eab7690d2a6ee69')
 )
 
