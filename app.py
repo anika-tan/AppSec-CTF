6057cba91ed037a4eac3352421b548bf19b0f11b
Bryan <bryanlu.my@gmail.com>
2024-10-16 17:03:03 +0800

    moar work

--- app.py
+++ app.py
@@ -71,70 +71,80 @@ def super_login():
     return render_template("superLogin.html", results=results, success=success)
 
 
-OWNER_SESSION_ID = None  # Change this to something else later
+# OWNER_SESSION_ID = "ze_session_id"  # Change this to something else later
+OWNER_SESSION_ID = None
 
 
 # Challenge 6 & 7
-@app.route("/ownerAccount", defaults={'subpath': ''}, methods=["GET"])
-@app.route("/ownerAccount/<path:subpath>", methods=["GET", "POST"])
-def owner_account(subpath):
+@app.route("/ownerAccount", methods=["GET"])
+@app.route("/ownerAccount/search", methods=["GET", "POST"])
+def owner_account():
     results = None
     if request.method == "POST":
-        if subpath == "activity":
-            session_id = request.cookies.get('session_id')
+        session_id = request.cookies.get('session_id')
 
-            # This is the table name
-            table_name = request.form['data']
+        # user input
+        request_query = request.form["search_query"]
 
-            # This is the type of data that the user request
-            request_fields = request.form['data_types']
-            sanitized_fields = [
-                field for field in request_fields if re.match(r'^[a-zA-Z\s]+$', field)]
+        # Connect to the database
+        conn = sqlite3.connect("database.db")
+        c = conn.cursor()
 
-            # Connect to the database
-            conn = sqlite3.connect("database.db")
-            c = conn.cursor()
+        # Check if session_id is correct or not
+        if session_id != OWNER_SESSION_ID:
+            abort(403)
 
-            # Check if session_id is correct or not
-            if session_id != OWNER_SESSION_ID:
-                abort(403)
+        c.execute(
+            f"SELECT name FROM ledger WHERE name LIKE '%{request_query}%'")
 
-            if len(sanitized_fields) == 0:
-                abort(403)
-
-            db_request_fields = ", ".join(sanitized_fields)
-
-            if table_name == "ledger":
-                c.execute(f"SELECT {db_request_fields} FROM ledger")
-                results = c.fetchall()
-            elif table_name == "sg_clients":
-                c.execute(f"SELECT {db_request_fields} FROM sg_clients")
-                results = c.fetchall()
-            elif request_query:
-                # sanitize the input by removing non-alphanumeric characters
-                sanitized_request_query = ''.join(
-                    e for e in request_query if re.match(r'^\w+$', e))
-                c.execute(
-                    f"SELECT names FROM sg_clients WHERE description LIKE '%{sanitized_request_query}%'")
-                results = c.fetchall()
-        elif subpath == 'search':
-            session_id = request.cookies.get('session_id')
-
-            # user input
-            request_query = request.form["search_query"]
-
-            # Connect to the database
-            conn = sqlite3.connect("database.db")
-            c = conn.cursor()
-
-            # Check if session_id is correct or not
-            if session_id != OWNER_SESSION_ID:
-                abort(403)
-
-            c.execute(
-                f"SELECT * FROM ledger WHERE description LIKE '%{request_query}%'")
+        results = c.fetchall()
 
     return render_template("owner_account.html", results=results)
+
+
+# Challenge 6 Insecure API Endpoint
+@app.route("/api/ownerAccount/activity", methods=["POST"])
+def owner_account_ledger():
+    session_id = request.cookies.get('session_id')
+
+    # This is the type of data that the user request
+    data = request.form.get('data')
+
+    if not re.match(r'^[a-zA-Z_\s]+$', data):
+        abort(403)
+    elif data == 'sg_clients':
+        results = {
+            "oioioi": """ [-] ERROR: Database not found
+            [!] LOG: Database under migration, see sg_clients_migration_asdfasdf(100 % completed)
+            [!] LOG: Database under maintenance(40 % completed)
+            [!] LOG: Please revisit us the next day"""
+        }
+        return {"results": results}
+
+    request_fields = request.form.getlist('data_types')
+
+    if (len(request_fields) == 0):
+        request_fields = ["*"]
+    sanitized_fields = [
+        field for field in request_fields if re.match(r'^[a-zA-Z\s\*]+$', field)]
+
+    # Connect to the database
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    # Check if session_id is correct or not
+    if session_id != OWNER_SESSION_ID:
+        abort(403)
+
+    if len(sanitized_fields) == 0:
+        abort(403)
+
+    db_request_fields = ", ".join(sanitized_fields)
+
+    c.execute(f"SELECT {db_request_fields} FROM {data}")
+    results = c.fetchall()
+
+    return {"results": results}
 
 
 if __name__ == "__main__":
